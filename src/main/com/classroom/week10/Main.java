ac726da4eb075d98867e8b17afe62e3741882f0b
Andranik Petrosyan <andranikpetrosyandev@gmail.com>
2022-03-14 20:52:36 +0400

    first part

---
+++ src/main/com/classroom/week10/Main.java
@@ -0,0 +1,41 @@
+package com.classroom.week10;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(1000);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        Iterator<Integer> iterator = list.iterator();
+
+
+        while (iterator.hasNext()) {
+            if (iterator.next() == 4) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println(list);
+
+        List<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        Iterator<Integer> iteratorLinked = list.iterator();
+
+
+        while (iteratorLinked.hasNext()) {
+            System.out.println(iteratorLinked.next());
+        }
+        System.out.println(linkedList);
+
+    }
+}
