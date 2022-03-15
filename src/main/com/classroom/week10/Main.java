26e2f81fa124b3bb0a4266cc56abf0a23661d8fa
Andranik Petrosyan <andranikpetrosyandev@gmail.com>
2022-03-15 12:22:57 +0400

    class

--- src/main/com/classroom/week10/Main.java
+++ src/main/com/classroom/week10/Main.java
@@ -7,35 +7,50 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(1000);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
+//        List<Integer> list = new ArrayList<>(1000);
+//        list.add(1);
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//
+//        Iterator<Integer> iterator = list.iterator();
+//
+//
+//        while (iterator.hasNext()) {
+//            if (iterator.next() == 4) {
+//                iterator.remove();
+//            }
+//        }
+//
+//        System.out.println(list);
+//
+//        List<Integer> linkedList = new LinkedList<>();
+//        linkedList.add(1);
+//        linkedList.add(2);
+//        linkedList.add(3);
+//        linkedList.add(4);
+//        Iterator<Integer> iteratorLinked = list.iterator();
+//
+//
+//        while (iteratorLinked.hasNext()) {
+//            System.out.println(iteratorLinked.next());
+//        }
+//        System.out.println(linkedList);
 
-        Iterator<Integer> iterator = list.iterator();
-
-
-        while (iterator.hasNext()) {
-            if (iterator.next() == 4) {
-                iterator.remove();
-            }
+        List<Integer> linkedNewList = new LinkedList<>();
+        for (int i = 0; i <= 100000; i++) {
+            linkedNewList.add(i);
         }
-
-        System.out.println(list);
-
-        List<Integer> linkedList = new LinkedList<>();
-        linkedList.add(1);
-        linkedList.add(2);
-        linkedList.add(3);
-        linkedList.add(4);
-        Iterator<Integer> iteratorLinked = list.iterator();
+        printAll(linkedNewList);
 
 
-        while (iteratorLinked.hasNext()) {
-            System.out.println(iteratorLinked.next());
+    }
+
+    public static void printAll(List<Integer> list) {
+        long start = System.currentTimeMillis();
+        for (Integer integer : list) {
+            System.out.println(integer);
         }
-        System.out.println(linkedList);
-
+        System.out.println((System.currentTimeMillis() - start));
     }
 }
